81ba4083a6f471dde51c916ea95e4ffeb0a0fee7
serieys <serieys.t@gmail.com>
2017-01-13 16:19:59 +0100

    change to global dir

---
+++ models/account.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    passportLocalMongoose = require('passport-local-mongoose')
+    ;
+//bcrypt = require('bcrypt-nodejs')
+
+var Account = new Schema({
+    username: String,
+    password: String,
+    admin: String
+});
+
+Account.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('Account', Account);
---
+++ routes.js
@@ -0,0 +1,57 @@
+var passport = require('passport');
+var Account = require('./models/account');
+var http = require('http');
+
+
+module.exports = function (app) {
+    
+  app.get('/', function (req, res) {
+      res.render('index', { user : req.user });
+  });
+
+  app.get('/register', function(req, res) {
+      res.render('register', { });
+  });
+
+  app.get('/listing', function(req, res) {
+      res.render('list', { });
+  });
+
+  app.post('/register', function(req, res) {
+      Account.register(new Account({ username : req.body.username, admin : req.body.admin}), req.body.password, function(err, account) {
+          if (err) {
+            return res.render("register", {info: "Sorry. That username already exists. Try again."});
+          }
+
+          passport.authenticate('local')(req, res, function () {
+            res.redirect('/chat');
+          });
+      });
+  });
+
+  app.get('/login', function(req, res) {
+      res.render('login', { user : req.user });
+  });
+
+  app.post('/login', passport.authenticate('local'), function(req, res) {
+      res.redirect('/chat');
+  });
+
+    app.get('/chat', function (req, res) {
+        res.render('chat', { user : req.user });
+        //res.sendfile('views/chat.html');
+    });
+
+  app.get('/logout', function(req, res) {
+      req.logout();
+      res.redirect('/');
+  });
+
+
+    /*
+  app.get('/ping', function(req, res){
+      res.send("pong!", 200);
+  });
+  */
+  
+};

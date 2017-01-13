feae254fe486d874d6df53528e3a9a561b647a84
serieys <serieys.t@gmail.com>
2017-01-13 10:50:22 +0100

    maj

--- testchattuto/server.js
+++ testchattuto/server.js
@@ -117,6 +117,15 @@ function getDateSendingMessage() {
 
 
 
+app.post('/login',
+    passport.authenticate('local'),
+    function(req, res) {
+        // If this function gets called, authentication was successful.
+        // `req.user` contains the authenticated user.
+        res.redirect('/users/' + req.user.username);
+    });
+
+
 
 
 
